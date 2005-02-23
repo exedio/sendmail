53b4ccd9d984cc0cf85c04c70b172681fa3f809b
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2005-02-23 14:32:57 +0000

    fix: do not create smtp session if there are no mails to be sent
    git-svn-id: https://svn.exedio.com/cope/trunk/sendmail@1775 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- src/com/exedio/sendmail/EmailRunner.java
+++ src/com/exedio/sendmail/EmailRunner.java
@@ -18,43 +18,47 @@ public final class EmailRunner
 	
 	public static final void sendMails(final EmailProvider provider, final int maximumResultSize)
 	{
-		final Properties properties = System.getProperties();
-		properties.put(MAIL_SMTP_HOST, provider.getSMTPHost());
-		properties.put("mail.transport.protocol", "smtp");
-		final Session session = Session.getInstance(properties);
-		session.setDebug(true);
-
 		final Collection emails = provider.getEmailsToBeSent(maximumResultSize);
-		for(Iterator i = emails.iterator(); i.hasNext(); )
+
+		if(!emails.isEmpty())
 		{
-			final EmailToBeSent email = (EmailToBeSent)i.next();
-			try
+			final Properties properties = System.getProperties();
+			properties.put(MAIL_SMTP_HOST, provider.getSMTPHost());
+			properties.put("mail.transport.protocol", "smtp");
+			final Session session = Session.getInstance(properties);
+			session.setDebug(true);
+
+			for(Iterator i = emails.iterator(); i.hasNext(); )
 			{
-				final MimeMessage message = new MimeMessage(session);
-				message.setFrom(new InternetAddress(email.getFrom()));
-				message.addRecipient(Message.RecipientType.TO, new InternetAddress(email.getTo()));
+				final EmailToBeSent email = (EmailToBeSent)i.next();
+				try
 				{
-					final String carbonCopy = email.getCarbonCopy();
-					if(carbonCopy!=null)
-						message.addRecipient(Message.RecipientType.CC, new InternetAddress(carbonCopy));
+					final MimeMessage message = new MimeMessage(session);
+					message.setFrom(new InternetAddress(email.getFrom()));
+					message.addRecipient(Message.RecipientType.TO, new InternetAddress(email.getTo()));
+					{
+						final String carbonCopy = email.getCarbonCopy();
+						if(carbonCopy!=null)
+							message.addRecipient(Message.RecipientType.CC, new InternetAddress(carbonCopy));
+					}
+					{
+						final String blindCarbonCopy = email.getBlindCarbonCopy();
+						if(blindCarbonCopy!=null)
+							message.addRecipient(Message.RecipientType.BCC, new InternetAddress(blindCarbonCopy));
+					}
+					{
+						final String subject = email.getSubject();
+						if(subject!=null)
+							message.setSubject(subject);
+					}
+					message.setText(email.getText());
+					Transport.send(message);
+					email.notifySent();
 				}
+				catch(MessagingException e)
 				{
-					final String blindCarbonCopy = email.getBlindCarbonCopy();
-					if(blindCarbonCopy!=null)
-						message.addRecipient(Message.RecipientType.BCC, new InternetAddress(blindCarbonCopy));
+					email.notifyFailed(e);
 				}
-				{
-					final String subject = email.getSubject();
-					if(subject!=null)
-						message.setSubject(subject);
-				}
-				message.setText(email.getText());
-				Transport.send(message);
-				email.notifySent();
-			}
-			catch(MessagingException e)
-			{
-				email.notifyFailed(e);
 			}
 		}
 	}
