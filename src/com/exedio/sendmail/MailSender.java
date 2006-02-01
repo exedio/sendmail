753c81a501916af4d5c95887609682a1043f5358
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2006-02-01 11:19:00 +0000

    close smtp connection in finally
    git-svn-id: https://svn.exedio.com/cope/trunk/sendmail@4782 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- src/com/exedio/sendmail/MailSender.java
+++ src/com/exedio/sendmail/MailSender.java
@@ -66,99 +66,108 @@ public final class MailSender
 			}
 			try
 			{
-				//final long start = System.currentTimeMillis();
-				transport.connect();
-				//System.out.println("Mailsender connected. ("+(System.currentTimeMillis()-start)+"ms)");
-			}
-			catch(MessagingException e)
-			{
-				throw new RuntimeException(e);
-			}
-			
-			for(Iterator i = mails.iterator(); i.hasNext(); )
-			{
-				final Mail mail = (Mail)i.next();
-				try
 				{
-					final MimeMessage message = new MimeMessage(session);
-					message.setFrom(new InternetAddress(mail.getFrom()));
-					{
-						final String[] to = mail.getTo();
-						if(to!=null)
-							for(int j = 0; j<to.length; j++)
-								message.addRecipient(Message.RecipientType.TO, new InternetAddress(to[j]));
-					}
-					{
-						final String[] carbonCopy = mail.getCarbonCopy();
-						if(carbonCopy!=null)
-							for(int j = 0; j<carbonCopy.length; j++)
-								message.addRecipient(Message.RecipientType.CC, new InternetAddress(carbonCopy[j]));
-					}
-					{
-						final String[] blindCarbonCopy = mail.getBlindCarbonCopy();
-						if(blindCarbonCopy!=null)
-							for(int j = 0; j<blindCarbonCopy.length; j++)
-								message.addRecipient(Message.RecipientType.BCC, new InternetAddress(blindCarbonCopy[j]));
-					}
-					{
-						final String subject = mail.getSubject();
-						if(subject!=null)
-							message.setSubject(subject, CHARSET);
-					}
-
-					final String text = mail.getText();
-					final DataSource[] attachments = mail.getAttachments();
-					if(attachments==null || attachments.length==0)
-					{
-						if(mail.isHTML())
-							message.setContent(text, HTML_CONTENT_TYPE);
-						else
-							message.setText(text, CHARSET);
-					}
-					else
-					{
-						final MimeMultipart multipart = new MimeMultipart("alternative");
-						{
-							final MimeBodyPart mainPart = new MimeBodyPart();
-							if(mail.isHTML())
-								mainPart.setContent(text, HTML_CONTENT_TYPE);
-							else
-								mainPart.setText(text, CHARSET);
-							mainPart.setDisposition(BodyPart.INLINE);
-							multipart.addBodyPart(mainPart);
-						}
-						for(int j = 0; j<attachments.length; j++)
-						{
-							final MimeBodyPart attachPart = new MimeBodyPart();
-							attachPart.setDataHandler(new DataHandler(attachments[j]));
-							attachPart.setDisposition(BodyPart.ATTACHMENT);
-							attachPart.setFileName(attachments[j].getName());
-							multipart.addBodyPart(attachPart);
-						}
-						message.setContent(multipart);
-					}
-					
 					//final long start = System.currentTimeMillis();
-					transport.sendMessage(message, message.getAllRecipients());
-					//System.out.println("Mailsender sent. ("+(System.currentTimeMillis()-start)+"ms)");
-					
-					mail.notifySent();
+					transport.connect();
+					//System.out.println("Mailsender connected. ("+(System.currentTimeMillis()-start)+"ms)");
 				}
-				catch(Exception e)
-				{
-					mail.notifyFailed(e);
-				}
-			}
 			
-			try
-			{
-				//final long start = System.currentTimeMillis();
-				transport.close();
-				//System.out.println("Mailsender closed. ("+(System.currentTimeMillis()-start)+"ms)");
+				for(Iterator i = mails.iterator(); i.hasNext(); )
+				{
+					final Mail mail = (Mail)i.next();
+					try
+					{
+						final MimeMessage message = new MimeMessage(session);
+						message.setFrom(new InternetAddress(mail.getFrom()));
+						{
+							final String[] to = mail.getTo();
+							if(to!=null)
+								for(int j = 0; j<to.length; j++)
+									message.addRecipient(Message.RecipientType.TO, new InternetAddress(to[j]));
+						}
+						{
+							final String[] carbonCopy = mail.getCarbonCopy();
+							if(carbonCopy!=null)
+								for(int j = 0; j<carbonCopy.length; j++)
+									message.addRecipient(Message.RecipientType.CC, new InternetAddress(carbonCopy[j]));
+						}
+						{
+							final String[] blindCarbonCopy = mail.getBlindCarbonCopy();
+							if(blindCarbonCopy!=null)
+								for(int j = 0; j<blindCarbonCopy.length; j++)
+									message.addRecipient(Message.RecipientType.BCC, new InternetAddress(blindCarbonCopy[j]));
+						}
+						{
+							final String subject = mail.getSubject();
+							if(subject!=null)
+								message.setSubject(subject, CHARSET);
+						}
+	
+						final String text = mail.getText();
+						final DataSource[] attachments = mail.getAttachments();
+						if(attachments==null || attachments.length==0)
+						{
+							if(mail.isHTML())
+								message.setContent(text, HTML_CONTENT_TYPE);
+							else
+								message.setText(text, CHARSET);
+						}
+						else
+						{
+							final MimeMultipart multipart = new MimeMultipart("alternative");
+							{
+								final MimeBodyPart mainPart = new MimeBodyPart();
+								if(mail.isHTML())
+									mainPart.setContent(text, HTML_CONTENT_TYPE);
+								else
+									mainPart.setText(text, CHARSET);
+								mainPart.setDisposition(BodyPart.INLINE);
+								multipart.addBodyPart(mainPart);
+							}
+							for(int j = 0; j<attachments.length; j++)
+							{
+								final MimeBodyPart attachPart = new MimeBodyPart();
+								attachPart.setDataHandler(new DataHandler(attachments[j]));
+								attachPart.setDisposition(BodyPart.ATTACHMENT);
+								attachPart.setFileName(attachments[j].getName());
+								multipart.addBodyPart(attachPart);
+							}
+							message.setContent(multipart);
+						}
+						
+						{
+							//final long start = System.currentTimeMillis();
+							transport.sendMessage(message, message.getAllRecipients());
+							//System.out.println("Mailsender sent. ("+(System.currentTimeMillis()-start)+"ms)");
+						}
+						
+						mail.notifySent();
+					}
+					catch(Exception e)
+					{
+						mail.notifyFailed(e);
+					}
+				}
 			}
 			catch(MessagingException e)
 			{
 				throw new RuntimeException(e);
+			}
+			finally
+			{
+				if(transport.isConnected())
+				{
+					try
+					{
+						//final long start = System.currentTimeMillis();
+						transport.close();
+						//System.out.println("Mailsender closed. ("+(System.currentTimeMillis()-start)+"ms)");
+					}
+					catch(MessagingException e)
+					{
+						throw new RuntimeException(e);
+					}
+				}
 			}
 		}
 	}
