1e2d9755d8d9c1254ccd0e425b91ddfacaee8354
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2008-04-07 12:21:00 +0000

    more explanation on getDate
    git-svn-id: https://svn.exedio.com/cope/trunk/sendmail@10017 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- src/com/exedio/sendmail/Mail.java
+++ src/com/exedio/sendmail/Mail.java
@@ -70,6 +70,21 @@ public interface Mail
 	/**
 	 * Returns the date the Date header of the mail should be set to.
 	 * According to RFC 2822 this Date should provide the creation date of the mail.
+	 * <p>
+	 * From the RFC:<br>
+	 * The origination date specifies the date and time at which the creator
+	 * of the message indicated that the message was complete and ready to
+	 * enter the mail delivery system.  For instance, this might be the time
+	 * that a user pushes the "send" or "submit" button in an application
+	 * program.  In any case, it is specifically not intended to convey the
+	 * time that the message is actually transported, but rather the time at
+	 * which the human or other creator of the message has put the message
+	 * into its final form, ready for transport.  (For example, a portable
+	 * computer user who is not connected to a network might queue a message
+	 * for delivery.  The origination date is intended to contain the date
+	 * and time that the user queued the message, not the time when the user
+	 * connected to the network to send the message.)
+	 * 
 	 * @return the creation date of the mail
 	 */
 	Date getDate();
