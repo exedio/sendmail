664667b76daa7c21216af57b83ab3081a41ecee4
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2013-04-02 08:08:00 +0000

    fix after r25541: move MailSenderProperties from demoshop into framework
    git-svn-id: https://svn.exedio.com/cope/sendmail/trunk@25542 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- src/com/exedio/sendmail/MailSenderProperties.java
+++ src/com/exedio/sendmail/MailSenderProperties.java
@@ -1,31 +1,27 @@
 /*
  * Copyright (C) 2004-2012  exedio GmbH (www.exedio.com)
  *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-package com.exedio.copedemo.feature.sendmail;
+package com.exedio.sendmail;
 
-import com.exedio.copedemo.feature.util.MyProperties;
+import com.exedio.cope.util.Properties;
 import com.exedio.sendmail.MailSender;
 
-public final class MailSenderProperties extends MyProperties
+public final class MailSenderProperties extends Properties
 {
 	private final String host = value("host", (String)null);
 	private final int port = value("port", 25, 0);
@@ -65,7 +61,7 @@ public final class MailSenderProperties extends MyProperties
 		super(source);
 	}
 
-	private static final class Auth extends MyProperties
+	private static final class Auth extends Properties
 	{
 		private final String username = value      ("username", (String)null);
 		private final String password = valueHidden("password", (String)null);
